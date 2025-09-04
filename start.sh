070e0a8f743234c235d77513e91f89b208d192e7
Robert <robert.aleks.zuk@gmail.com>
2025-09-04 08:40:49 +0200

    Fix npm build - remove from runtime, add verbose logging

--- start.sh
+++ start.sh
@@ -7,9 +7,13 @@ export APP_DEBUG=true
 cd /var/www/html
 
 # Wyczyść cache Laravel
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+
+# Wygeneruj komponenty Blade Icons
+php artisan icons:cache
 
 # Start Nginx
 service nginx start
